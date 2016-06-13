e03057d2c51eb6da59df14dcb505a56a6fcd3f73
Richard Cornish <richardcornish@gmail.com>
2016-06-13 14:27:37 -0500

    Added color count, total, and percentage

--- colorthief.py
+++ colorthief.py
@@ -352,9 +352,9 @@ class CMap(object):
     def __init__(self):
         self.vboxes = PQueue(lambda x: x['vbox'].count * x['vbox'].volume)
 
-    @property
     def palette(self):
-        return self.vboxes.map(lambda x: x['color'])
+        total = sum(self.vboxes.map(lambda x: x['vbox'].count))
+        return self.vboxes.map(lambda x: x['color'] + (x['vbox'].count, total, int(x['vbox'].count / float(total) * 100)))
 
     def push(self, vbox):
         self.vboxes.push({
